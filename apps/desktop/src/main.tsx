5c0c469cb9a251f3767823a37699dd02d91e690b
huoago <135907394+huoago@users.noreply.github.com>
2026-09-11 10:47:54 -0500

    ui: load Phase 11 styles

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -8,7 +8,7 @@ import "./phase4.css";
 import "./phase5.css";
 import "./phase6.css";
 import "./phase7.css";
-import "./phase8.css";
+import "./phase8.css";\nimport "./phase11.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
